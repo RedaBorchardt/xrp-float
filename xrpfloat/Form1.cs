b9c553f9aa0b2df3d26093474cd7acc1705668f6
Reda Borchardt <redaborchardt@gmail.com>
2018-12-27 11:49:49 +0200

    Add files via upload

---
+++ xrpfloat/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xrpfloat
+{
+    public partial class Form1 : Form
+    {
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                // turn on WS_EX_TOOLWINDOW style bit
+                cp.ExStyle |= 0x80;
+                return cp;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.LimeGreen;
+            this.TransparencyKey = Color.LimeGreen;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            this.TopMost = true;
+            this.Top =  0;
+            this.Left = -3;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TickerSearch.UpdateTicker(this.label1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TickerSearch.UpdateTicker(this.label1);
+
+            ContextMenu cm = new ContextMenu();
+            cm.MenuItems.Add("Exit", new EventHandler(Exit_Click));
+            this.label1.ContextMenu = cm;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ xrpfloat/search.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xrpfloat
+{
+    public class TickerSearch
+    {
+        public static async void UpdateTicker(System.Windows.Forms.Label t)
+        {
+            RootObject result = await GetTicker();
+            if (result != null)
+            {
+                t.Text = "XRP: " + result.ask;
+                if (float.Parse(result.last_price)-float.Parse(result.ask) < 0)
+                {
+                    t.BackColor = System.Drawing.Color.ForestGreen;
+                } else
+                {
+                    t.BackColor = System.Drawing.Color.Firebrick;
+                }
+            }
+        } 
+
+        public static async Task<RootObject> GetTicker()
+        {
+            string apiUrl = "https://api.bitfinex.com/v1/pubticker/xrpusd";
+
+            using (var client = new HttpClient())
+            {
+                string repUrl = apiUrl;
+                HttpResponseMessage response = await client.GetAsync(repUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    var rootResult = JsonConvert.DeserializeObject <RootObject>(result);
+                    return rootResult;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
